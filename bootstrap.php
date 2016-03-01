35af80fcdd1a48ba0fc1a7aee5568fb1bdb51958
David Norminton <davidnorminton@gmail.com>
2016-03-01 08:07:43 +0000

    Added files via upload

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,8 @@
 <?php
+// start a session if one hasn't already been created
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 // uncomment in development mode
 error_reporting(E_ALL);
 
@@ -7,16 +11,17 @@ define("DS", DIRECTORY_SEPARATOR);
 // load autoloader script
 include("nano/autoload.php");
 
-// full path from root
+// Define a constant to represent the full path from root
 define("APP_PATH", dirname(__FILE__) . DS);
 
 // define required paths for controllers, models and views
-define("CONTROLLERS_PATH", APP_PATH."controllers".DS);
-define("MODELS_PATH", APP_PATH."models".DS);
-define("MVC_VIEW_PATH", APP_PATH."views".DS);
-define("THEME_PATH", APP_PATH."themes".DS);
+define("CONTROLLERS_PATH", APP_PATH . "nano" . DS . "controllers" . DS);
+define("MODELS_PATH", APP_PATH . "nano" . DS . "models" . DS);
+define("MVC_VIEW_PATH", APP_PATH . "nano" . DS . "views" . DS);
+define("THEME_PATH", APP_PATH . "nano" . DS . "themes" . DS);
+define("THEME", "\\nano\\themes\\original\\theme");
 // run main controller    
-\core\controller::run();
+$controller = new \core\controller();
 
         
 
