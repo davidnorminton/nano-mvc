10f710a3180607a5027064785d938e3c16420e72
David Norminton <davidnorminton@gmail.com>
2016-03-10 03:59:47 +0000

    added cache usage

--- nano/controllers/index.php
+++ nano/controllers/index.php
@@ -9,23 +9,23 @@ class index extends \core\controller
 {
     // instance of model 
     private $model;
-
-    //private $theme;    
+    
+    private $page;
     
     public function __construct($params)
     {
-
+        // create instance of model and pass any parameters
         $this->model = new \nano\models\index($param[0]);
-
-        // pass model and theme objects to view object
-
+        // pass an instance of model as parameter to view 
         $view = new \nano\views\index($this->model);
         $view_build = $view->build();
-        // theme is set in bootstrap.php
-        $use_theme = THEME_NAMESPACE ;
-        // create an instance of template class
-        $this->theme = new $use_theme();
-        $this->theme->setTitle($this->model->getTitle());
-        $this->theme->render($view_build);        
+        // get them from parent class
+        $theme = $this->getTheme();
+        // theme will set header and footer variables
+        $theme->setTitle($this->model->getTitle());
+        // pass view output to theme to be rendered with header and footer       
+        $this->page = $theme->render($view_build);
+        // create a cache file of contents
+        $this->cacheFile($theme->getCache());
     }
 } 
