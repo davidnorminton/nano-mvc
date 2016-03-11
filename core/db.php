b24d3e4685341f0b95e330f7fd334bd8f356f919
David Norminton <davidnorminton@gmail.com>
2016-03-11 04:29:46 +0000

    database connection : mysqli

---
+++ core/db.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * class to handle database connection and some common functions
+ * change username, password, database, and server to correct settings
+ */
+namespace core;
+
+class db{
+
+    private $user = 'root';
+   
+    private $password = 'root';
+    
+    private $server = 'localhost';
+    
+    private $db_name = '';
+    
+
+    public function __construct()
+    {
+        // Create connection
+        $conn = new \mysqli($this->server, $this->user, $this->password, $this->db_name);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            echo "error";
+      } 
+   }
+} 
