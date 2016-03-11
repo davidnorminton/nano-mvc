563390dfc2dd0906cf7d1d670b53b917bda7fe84
David Norminton <davidnorminton@gmail.com>
2016-03-11 02:19:14 +0000

    added a few more constants

--- bootstrap.php
+++ bootstrap.php
@@ -6,24 +6,26 @@ if (session_status() == PHP_SESSION_NONE) {
 // uncomment in development mode
 error_reporting(E_ALL);
 
-define("DS", DIRECTORY_SEPARATOR);
+define('DS', DIRECTORY_SEPARATOR);
 
 // load autoloader script
-include("nano/autoload.php");
+include('nano/autoload.php');
 
 // Define a constant to represent the full path from root
 define("APP_PATH", dirname(__FILE__) . DS);
 
 // define required paths for controllers, models and views
-define("CONTROLLERS_PATH", APP_PATH . "nano" . DS . "controllers" . DS);
-define("MODELS_PATH", APP_PATH . "nano" . DS . "models" . DS);
-define("MVC_VIEW_PATH", APP_PATH . "nano" . DS . "views" . DS);
-define("THEME", "original");
-define("THEME_PATH", APP_PATH . "nano" . DS . "themes" . DS . THEME . DS);
-define("THEME_HEADER", THEME_PATH . "header.phtml");
-define("THEME_FOOTER", THEME_PATH . "footer.phtml");
-define("THEME_NAMESPACE", "\\core\\theme");
-define("CACHE", True);
+define('CONTROLLERS_PATH', APP_PATH . 'nano' . DS . 'controllers' . DS);
+define('MODELS_PATH', APP_PATH . 'nano' . DS . 'models' . DS);
+define('MVC_VIEW_PATH', APP_PATH . 'nano' . DS . 'views' . DS);
+define('THEME', 'original');
+define('THEME_PATH', APP_PATH . 'nano' . DS . 'themes' . DS . THEME . DS);
+define('THEME_HEADER', THEME_PATH . 'header.phtml');
+define('THEME_FOOTER', THEME_PATH . 'footer.phtml');
+define('THEME_NAMESPACE', "\\core\\theme");
+define('CACHE', True);
+define('CACHE_PATH', APP_PATH . 'nano' . ds . 'cache' . ds);
+define('DEFAULT_CONTROLLER', 'index');
 $class = new \core\frontcontroller();  
 
         
