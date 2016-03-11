6cf42743a21b49760a8d7fb3033f5a2b5eaf37e2
David Norminton <davidnorminton@gmail.com>
2016-03-11 03:13:59 +0000

    bug fixes

--- core/controller.php
+++ core/controller.php
@@ -21,16 +21,28 @@ class controller {
     
     public function cacheFile($contents)
     {
+
         $url = $_SERVER['REQUEST_URI'];
         $url = str_replace('/', '', $url);
-        $fp = '/var/www/nano/cache/'.$url.'.html';
-        if (is_writable('/var/www/nano/cache')) {
-            
+        // cache file path
+        $fp = CACHE_PATH . $url . '.html';
+        if (is_writable(CACHE_PATH)) {
+           // create file and make ready to write 
            $myfile = fopen($fp, "w") or die("Unable to open file!");
+           // give correct permissions
            chmod($myfile, 0644);
+           // write contents
            fwrite($myfile, $contents);
            fclose($myfile);
         }   
+    }
+    
+    public function __destruct()
+    {
+        if ( $this->theme == NULL)
+           throw new Exception("class of type theme not instansiated");
+        // create a cache file of contents
+            $this->cacheFile($this->theme->getCache()); 
     }
 
 }
