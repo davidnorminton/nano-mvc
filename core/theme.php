4be42f403bf10608d4e71ca41eefb03c947de211
David Norminton <davidnorminton@gmail.com>
2016-03-12 22:39:38 +0000

    added better documentation

--- core/theme.php
+++ core/theme.php
@@ -1,10 +1,18 @@
 <?php
+/**
+ * main template class
+ *
+ */
 namespace core;
 
 class theme{
-	protected $header = THEME_HEADER,
-	          $footer = THEME_FOOTER;
+    // header html to be used set in bootstrap
+	protected $header = THEME_HEADER;
+	// footer html to be used set in bootstrap
+	protected $footer = THEME_FOOTER;
+	// <head><title>?</title></head>
 	private $title;
+	// strores paresed html of page
 	private $cache;
 
 	
@@ -12,21 +20,37 @@ class theme{
 		return $this->$name;
 	}
 
-
+    /**
+     * method render
+     * @param string $view - paresed html of page body
+     * renders header, footer with page body
+     * if cache is set to on will store a string of page html in
+     * $var cache 
+     */
 	public function render($view){
 		ob_start();
 		include($this->header);
 		echo $view;
 		include($this->footer);
-		$this->cache = ob_get_contents();
+		// store contents of page to be used in cache
+		if (CACHE == True) {
+		    $this->cache = ob_get_contents();
+		}
 		ob_end_flush();
 	}
 	
+	/**
+	 * method setTitle - set page title for seo and browser tab
+	 * @param string $title
+	 */
 	public function setTitle($title)
 	{
 	  $this->title = $title;
 	}
-	
+
+    /**
+     * method getCache - returns a string containing the parsed html of page
+     */	
 	public function getCache()
 	{
 	  return $this->cache;
