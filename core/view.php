fe8c1ab9ac1416bcd8d1b6f7ac72dc8d7404c1da
David Norminton <davidnorminton@gmail.com>
2016-03-12 22:39:51 +0000

    added better documentation

--- core/view.php
+++ core/view.php
@@ -1,13 +1,20 @@
 <?php
 /**
- * Autoload for any objects that are instansitaed within a class that extends
- * this view class
+ * core view class
+ * 
  */
 namespace core;
 
 class view
 {
 
+    /**
+     * render method
+     * @param array $vars - containd data to be added to html
+     * @param string $html - file within template folder to be used
+     * note - path to template folder set in bootstrap file
+     * return a string containing the parsed html
+     */
     public function render($vars, $html)
     {
          ob_start();
