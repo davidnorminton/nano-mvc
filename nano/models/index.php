1a4155edbe88b9ba763619b97491dad9276b30f2
David Norminton <davidnorminton@gmail.com>
2016-03-12 23:32:18 +0000

    Added files via upload

--- nano/models/index.php
+++ nano/models/index.php
@@ -7,22 +7,22 @@ namespace nano\models;
 
 class index extends \core\model
 {
-    private $title = "This is the page title";
+    private $title = "Nano mvc test page";
     
-    private $vars, $inc;
+    private $vars;
     
     private $articles = array(
-                              ['title'=>"This is the first title",
-                               'body'=>'Below is a random image',
-                               'img'=> 'http://www.passmark.com/images/monitortestscreenshot5.gif'],
-                              ['title'=>"This is the second title", 
-                               'body'=>'Below is another random image',
-                               'img'=>'http://www.passmark.com/images/monitortestscreenshot3.jpg']
+                              ['title'=>"This is foo",
+                               'body'=>'Pellentesque odio sem, aliquam sed viverra placerat, placerat ut leo. Nam laoreet vehicula risus, sed pharetra purus lacinia at. Etiam ac semper urna, maximus ultrices nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque nec mollis eros. Ut ut est est. Curabitur congue pellentesque urna id pulvinar. Suspendisse sollicitudin a ex a convallis. Ut vitae odio commodo, eleifend odio id, pulvinar lacus. Curabitur tellus nisi, lacinia vitae sagittis in, dapibus vitae nibh. Vivamus varius, metus a blandit molestie, dolor risus eleifend orci, nec porttitor mi felis at quam. Curabitur blandit massa in rutrum facilisis. Proin lacinia accumsan imperdiet.',
+                               'img'=> '/images/foo.jpg'],
+                              ['title'=>"This is bar", 
+                               'body'=>'Nullam ullamcorper neque eu tincidunt volutpat. Morbi tincidunt rutrum velit. Praesent non luctus nulla, et condimentum neque. Donec fringilla risus lacus, at vestibulum diam tincidunt eget. Donec in porta mauris. In hac habitasse platea dictumst. Curabitur mi nisl, blandit at tempus eu, sollicitudin vel lacus. Sed facilisis, justo ullamcorper ullamcorper dignissim, neque risus cursus leo, non euismod orci dolor at lectus. Vivamus suscipit ante vel magna vehicula, non ornare erat faucibus.',
+                               'img'=>'/images/bar.jpg']
                              );
     
-    public function __construct($inc, $param)
+    public function __construct($param)
     {
-     $this->inc = $inc;
+
      $this->vars = $param;
     }  
     
@@ -36,9 +36,5 @@ class index extends \core\model
       return $this->articles;
     }
     
-    public function getInc()
-    {
-       return $this->inc;
-    }
      
 } 
