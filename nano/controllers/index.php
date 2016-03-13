6583b7b6fe51fd65e79d399226eb4059fa500ab3
David Norminton <davidnorminton@gmail.com>
2016-03-13 00:21:14 +0000

    Added files via upload

--- nano/controllers/index.php
+++ nano/controllers/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Default index page of website
+ * Default index page of test website
  *
  */
 namespace nano\controllers;
@@ -10,22 +10,24 @@ class index extends \core\controller
     // instance of model 
     private $model;
     
-    private $page;
     
     public function __construct($params)
     {
+
         // create instance of model and pass any parameters
-        $this->model = new \nano\models\index($param[0]);
+        $this->model = new \nano\models\index($params);
+        
         // pass an instance of model as parameter to view 
         $view = new \nano\views\index($this->model);
-        $view_build = $view->build();
-        // get them from parent class
-        $theme = $this->getTheme();
+        
+        // get theme from parent class
+        $this->theme = $this->getTheme();
+        
         // theme will set header and footer variables
-        $theme->setTitle($this->model->getTitle());
+        $this->theme->setTitle($this->model->getTitle());
+        
         // pass view output to theme to be rendered with header and footer       
-        $this->page = $theme->render($view_build);
-        // create a cache file of contents
-        $this->cacheFile($theme->getCache());
+        $this->theme->render($view->build());
+
     }
 } 
