8ffe170a6676c1155a2a11655348d247b361dad8
David Norminton <davidnorminton@gmail.com>
2016-03-13 00:21:56 +0000

    Added files via upload

--- nano/views/index.php
+++ nano/views/index.php
@@ -1,22 +1,36 @@
 <?php
-
+/**
+ * test page
+ */
 namespace nano\views;
 
-class index extends \core\view
-{
+
+class index extends \core\view {
+
+    // an instance of the model used
     private $model;
-    
+    // an array of data used to parse varibaes on the html page
     private $vars;
     
+    /**
+     * method construct - loads the model instance into the $model property
+     * Ensure the model is of type model
+     */
     public function __construct(\nano\models\index $model)
     {
      $this->model = $model;
     }
     
+    /**
+     * method build - sets elements into the $vars array getting data from model
+     *
+     */
     public function build()
     {
      $this->vars['title'] = $this->model->getTitle();
      $this->vars['articles'] = $this->model->getArticles();
+     // render method is in the \core\view class
+     // used to build the page contents
      return $this->render( $this->vars, THEME_PATH . "article.phtml");   
     }
 }
