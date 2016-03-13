c4a3793fe689e40cdc154e7e9537476d3cf48580
David Norminton <davidnorminton@gmail.com>
2016-03-13 00:21:35 +0000

    Added files via upload

--- nano/models/index.php
+++ nano/models/index.php
@@ -1,16 +1,17 @@
 <?php
 /**
- * Model for index 
+ * Model for index test page
  *
  */
 namespace nano\models;
 
 class index extends \core\model
-{
+{ 
+    // main page title
     private $title = "Nano mvc test page";
-    
+    // array that holds parameters extracted from uri
     private $vars;
-    
+    // array of articles 
     private $articles = array(
                               ['title'=>"This is foo",
                                'body'=>'Pellentesque odio sem, aliquam sed viverra placerat, placerat ut leo. Nam laoreet vehicula risus, sed pharetra purus lacinia at. Etiam ac semper urna, maximus ultrices nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque nec mollis eros. Ut ut est est. Curabitur congue pellentesque urna id pulvinar. Suspendisse sollicitudin a ex a convallis. Ut vitae odio commodo, eleifend odio id, pulvinar lacus. Curabitur tellus nisi, lacinia vitae sagittis in, dapibus vitae nibh. Vivamus varius, metus a blandit molestie, dolor risus eleifend orci, nec porttitor mi felis at quam. Curabitur blandit massa in rutrum facilisis. Proin lacinia accumsan imperdiet.',
@@ -20,17 +21,25 @@ class index extends \core\model
                                'img'=>'/images/bar.jpg']
                              );
     
+    
     public function __construct($param)
     {
 
      $this->vars = $param;
     }  
     
+    /**
+     * method getter getTile
+     * returns page title 
+     */
     public function getTitle()
     {
       return $this->title;
     }
     
+    /**
+     * method getter getArticles - return array of artiles
+     */
     public function getArticles()
     {
       return $this->articles;
