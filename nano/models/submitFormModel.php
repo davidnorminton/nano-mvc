ae8d6b8f0b9fe8e819b232d57c3d9fe891fde295
David Norminton <davidnorminton@gmail.com>
2016-03-16 04:57:25 +0000

    Added files via upload

--- nano/models/submitFormModel.php
+++ nano/models/submitFormModel.php
@@ -11,17 +11,33 @@ class submitFormModel extends \core\model
     private $title = "Submitted form data";
     // array that holds parameters extracted from uri
     private $vars;
-    
+    // store email object
+    private $email;
+    // return message
+    private $return_msg;
     
     public function __construct()
     {
   
-     $this->vars['name'] = $_POST['name'];
-     $this->vars['website'] = $_POST['website'];
-     $this->vars['email'] = $_POST['email'];
-     $this->vars['message'] = $_POST['message'];
-                    
+        $this->vars['name'] = $_POST['name'];
+        $this->vars['website'] = $_POST['website'];
+        $this->vars['email'] = $_POST['email'];
+        $this->vars['message'] = $_POST['message'];
+        $this->email = new \core\email($this->vars['email'],
+                                       $this->vars['message'],
+                                       'Sent from contact form');
+        if ( $this->email->send()) {
+           $this->return_msg = "Successfully sent";
+        } else {
+           $this->return_msg = "Problem sending message";
+        }                                              
     }  
+    
+    public function getReturnMsg()
+    {
+        return $this->return_msg;
+    }
+    
     
     /**
      * method getter getTile
