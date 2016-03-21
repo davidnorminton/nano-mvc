5d634b71402b73c9401a03e17703e83e398f7e18
David Norminton <davidnorminton@gmail.com>
2016-03-21 06:13:26 +0000

    Update redirect.php

--- core/redirect.php
+++ core/redirect.php
@@ -6,11 +6,18 @@ namespace core;
 
 class redirect {
 
-    public function __construct($url, $message)
+    public function __construct($url, $query=NULL, $message=NULL)
     {
-        $error = urlencode($message);
-        $msg = $url . '?error=' . $error;
-        die(header("Location: $msg"));
+        if ($msg != NULL) {    
+           $msg = urlencode($message);
+        }
+        if ($query) {
+           $query = 'error';
+        }
+        if ($query == NULL || $query == 'error') {
+            $message = $url . '?'.$query.'=' . $msg;
+            die(header("Location: $message"));
+        }
     }
 
 } 
